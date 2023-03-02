f8e060181e31421b24fc9facc8b16dca0f28faf2
exit <saraswathi.mac@gmail.com>
2023-03-02 18:45:38 +0530

    Added changes to files

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { useFetch } from "./hooks/useFetch";
-import Follower from "./components/Follower";
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
+// import components
+import Navbar from "./components/Navbar";
 function App() {
-  const { loading, data } = useFetch();
-  const [page, setPage] = useState(0);
-  const [followers, setFollowers] = useState([]);
-
-  useEffect(() => {
-    if (loading) return;
-    setFollowers(data[page]);
-  }, [loading, page]);
-
-  const nextPage = () => {
-    setPage((oldPage) => {
-      let nextPage = oldPage + 1;
-      if (nextPage > data.length - 1) {
-        nextPage = 0;
-      }
-      return nextPage;
-    });
-  };
-
-  const prevPage = () => {
-    setPage((oldPage) => {
-      let prevPage = oldPage - 1;
-      if (prevPage < 0) {
-        prevPage = data.length - 1;
-      }
-      return prevPage;
-    });
-  };
-
-  const handlePage = (index) => {
-    setPage(index);
-  };
-
-  const getStorageTheme = () => {
-    let theme = 'light-theme';
-    if (localStorage.getItem('theme')) {
-      theme = localStorage.getItem('theme');
-    }
-    return theme;
-  };
-
-  const [theme, setTheme] = useState(getStorageTheme());
-
-  const toggleTheme = () => {
-    if (theme === 'light-theme') {
-      setTheme('dark-theme');
-    } else {
-      setTheme('light-theme');
-    }
-  };
-
-  useEffect(() => {
-    document.documentElement.className = theme;
-    localStorage.setItem('theme', theme);
-  }, [theme]);
-
   return (
-    <main>
-      
-      <div className="section-title">
-        <h1>{loading ? "loading..." : "pagination"}</h1>
-        <div className="underline"></div>
-      </div>
-      <button className="btn-toggle" onClick={toggleTheme}>
-            toggle
-          </button>
-      <section className="followers">
-        <div className="container">
-          {followers.map((follower) => {
-            return <Follower key={follower.id} {...follower} />;
-          })}
-        </div>
-        {!loading && (
-          <div className="btn-container">
-            <button className="prev-btn" onClick={prevPage}>
-              prev
-            </button>
-            {data.map((item, index) => {
-              return (
-                <button
-                  key={index}
-                  className={`page-btn ${index === page ? "active-btn" : null}`}
-                  onClick={() => handlePage(index)}
-                >
-                  {index + 1}
-                </button>
-              );
-            })}
-            <button className="next-btn" onClick={nextPage}>
-              next
-            </button>
-          </div>
-        )}
-      </section>
-    </main>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cocktail/:id" element={<SingleCocktail />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
