e3722723aeecdd10dd09da9a6b56a4880089d970
exit <saraswathi.mac@gmail.com>
2023-03-02 18:59:46 +0530

    Updated files of Components

--- src/components/CocktailList.jsx
+++ src/components/CocktailList.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
 
-export default function CocktailList() {
-  const { cocktails, loading } = useGlobalContext()
-  if (loading) {
-    return <Loading/>
-  }
-  if (cocktails.length < 1) {
-    return (
-      <h2 className='section-title'>
-        no cocktails matched your search criteria
-      </h2>
-    )
-  }
+const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext();
+
   return (
-    <section className='section'>
-      <h2 className='section-title'>cocktails</h2>
-      <div className='cocktails-center'>
-        {cocktails.map((item) => {
-          return <Cocktail key={item.id} {...item} />
-        })}
-      </div>
-    </section>
-  )
+    <>
+      {loading ? <Loading /> : ""}
+
+      {cocktails.length < 1 ? (
+        <h2 className="section-title">
+          no cocktails matched your search criteria
+        </h2>
+      ) : (
+        ""
+      )}
+
+      <section className="section">
+        <h2 className="section-title">cocktails</h2>
+        <div className="cocktails-center">
+          {cocktails.map((item) => {
+            return <Cocktail key={item.id} {...item} />;
+          })}
+        </div>
+      </section>
+    </>
+  );
 }
+
+export default CocktailList;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../logo.svg'
-export default function Navbar() {
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
+
+const Navbar = () => {
   return (
-    <nav className='navbar'>
-      <div className='nav-center'>
-        <Link to='/'>
-          <img src={logo} alt='cocktail db logo' className='logo' />
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to="/">
+          <img src={logo} alt="cocktail db logo" className="logo" />
         </Link>
-        <ul className='nav-links'>
+        <ul className="nav-links">
           <li>
-            <Link to='/'>home</Link>
+            <Link to="/">home</Link>
           </li>
           <li>
-            <Link to='/about'>about</Link>
+            <Link to="/about">about</Link>
           </li>
         </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
+
+export default Navbar;
