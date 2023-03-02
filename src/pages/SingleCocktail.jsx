1b0d0d7bd3f4a3d15b688f765ad9726d62fafccb
exit <saraswathi.mac@gmail.com>
2023-03-02 19:25:25 +0530

    Updated on the files

--- src/pages/SingleCocktail.jsx
+++ src/pages/SingleCocktail.jsx
@@ -57,8 +57,8 @@ export default function SingleCocktail() {
     getCocktail();
   }, [id]);
 
-  if (loading) {
-    return <Loading />;
+  {
+    loading ? <Loading /> : "";
   }
 
   if (!cocktail) {
