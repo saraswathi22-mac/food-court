9e8c74d0bcd8d442b3b532de67415d1e2683e24d
exit <saraswathi.mac@gmail.com>
2024-02-19 17:29:18 +0530

    Added Log out

--- app/page.js
+++ app/page.js
@@ -1,9 +1,21 @@
-import Image from 'next/image'
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { data: session } = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    if (!session?.user) {
+      router.push("/Login");
+    }
+  }, [session]);
   return (
     <div>
       <h2>Subscribe</h2>
+      <button onClick={() => signOut()}>SignOut</button>
     </div>
-  )
+  );
 }
