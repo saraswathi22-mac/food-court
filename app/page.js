7562a912defd05b7584711f0b7088baa4c492a67
exit <saraswathi.mac@gmail.com>
2024-02-20 21:21:16 +0530

    Added view of google map

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import CategoryList from "@/components/Home/CategoryList";
+import GoogleMapsView from "@/components/Home/GoogleMapsView";
 import RangeSelect from "@/components/Home/RangeSelect";
 import SelectRating from "@/components/Home/SelectRating";
 import { signOut, useSession } from "next-auth/react";
@@ -15,7 +16,7 @@ export default function Home() {
       router.push("/Login");
     }
   }, [session]);
-  
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-4">
       <div className="p-3">
@@ -23,7 +24,9 @@ export default function Home() {
         <RangeSelect />
         <SelectRating />
       </div>
-      <div className="col-span-3">Second</div>
+      <div className="col-span-3">
+        <GoogleMapsView />
+      </div>
     </div>
   );
 }
