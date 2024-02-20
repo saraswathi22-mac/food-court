8e5daf17cc0808a6b0e5854b9320eeca7630eddd
exit <saraswathi.mac@gmail.com>
2024-02-20 16:06:47 +0530

    Updated categorylist

--- components/Home/CategoryList.js
+++ components/Home/CategoryList.js
@@ -11,6 +11,7 @@ const CategoryList = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {categoryList.map((item, index) => (
           <div
+            key={index}
             className={`flex flex-col justify-center items-center bg-gray-100 p-2 m-2 rounded-lg grayscale hover:grayscale-0 cursor-pointer border-purple-400 ${
               selectedCategory === index ? "grayscale-0 border-[1px]" : null
             }`}
