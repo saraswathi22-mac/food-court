69ed85d55c968d27441d299e77e1ab1c09950aea
exit <saraswathi.mac@gmail.com>
2024-02-29 22:37:30 +0530

    Displaying user location

--- components/Home/GoogleMapsView.js
+++ components/Home/GoogleMapsView.js
@@ -1,20 +1,33 @@
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
-import React from "react";
+"use client";
+
+import { UserLocationContext } from "@/context/UserLocationContext";
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
+import React, { useContext } from "react";
 
 const GoogleMapsView = () => {
+  const { userLocation, setUserLocation } = useContext(UserLocationContext);
+
   const containerStyle = {
     width: "100%",
     height: "70vh",
   };
-  const coordinate = { lat: -34.397, lng: 150.644 };
+
   return (
     <div>
       <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}>
         <GoogleMap
           mapContainerStyle={containerStyle}
-          center={coordinate}
+          center={userLocation}
           zoom={10}
-        ></GoogleMap>
+        >
+          <MarkerF
+            position={userLocation}
+            icon={{
+              url: "/user-location.png",
+              scaledSize: { width: 50, height: 50 },
+            }}
+          />
+        </GoogleMap>
       </LoadScript>
     </div>
   );
